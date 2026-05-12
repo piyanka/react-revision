79e0564cac49157e40c07efc5594b009547a9975
Priyanka Kumari <priyanka@Priyankas-MacBook-Air.local>
2026-05-13 02:47:49 +0530

    Revise useState hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
 
 function App() {
+  let [counter, setCounter] = useState(5);
+
+  const IncrementCounter = () => {
+    setCounter(counter + 1);
+  }
+
+  const DecrementCounter = () => {
+    setCounter(counter - 1);
+  }
+  
   return (
-    <h1>Priyanka Kumari</h1>
+    <div>
+      <h2>Updates Counter values on Clicking on Respectives Buttons</h2>
+      <h3>Counter Value : {counter}</h3>
+
+      <button onClick={IncrementCounter}>Increase Counter</button>
+      <br /><br />
+      <button onClick={DecrementCounter}>Decrease Counter</button>
+    </div>
+    
   );
 }
 
