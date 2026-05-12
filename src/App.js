bc1bd35d3fbd259df1c9815b264e73c92647ff88
Priyanka Kumari <priyanka@Priyankas-MacBook-Air.local>
2026-05-13 00:14:27 +0530

    Removed Unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h>Priyanka Kumari</h>
-    </div>
+    <h1>Priyanka Kumari</h1>
   );
 }
 
